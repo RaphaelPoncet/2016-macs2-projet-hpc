577333cfff00f55538306d7d160939c6aa644c48
Raphaël Poncet <raphael.poncet@gmail.com>
2016-05-05 16:28:48 +0200

    Activate logging

--- main.cpp
+++ main.cpp
@@ -9,13 +9,34 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language goveroning permissions and
 // limitations under the License.
+
+#include "plog/Log.h"
+#include "plog/Appenders/ConsoleAppender.h"
+#include "plog/Appenders/RollingFileAppender.h"
 
 #include "multidimensional_storage.hpp"
 
 int main(int argc, char** argv) {
 
+  // Initialize logging.
+  const bool log_to_file = false;
+
+  if (log_to_file) {
+
+    const int nb_rolling_logfiles = 2;
+    const int max_logfile_size = 1000000;
+    plog::init(plog::verbose, "wave_propagator.log", max_logfile_size, nb_rolling_logfiles);
+
+  } else {
+
+    static plog::ConsoleAppender<plog::TxtFormatter> console_appender;
+    plog::init(plog::verbose, &console_appender);
+
+  }
+
+  LOG_INFO << "Hello from wave propagator!";
 
   return 0;
 
